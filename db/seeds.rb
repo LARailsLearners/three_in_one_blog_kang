cd4be15aba1bb5407e93c21a875753142976ec28
kangkyu <kangkyu1111@gmail.com>
2015-08-21 10:12:39 -0700

    add seeds.rb for title and body with paragraphs - generated ipsum

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.create!([{
+  title: "Hipster Ipsum", 
+  body: "Odd Future fanny pack next level Etsy. High Life biodiesel literally, 8-bit lo-fi kitsch Vice pop-up crucifix you probably haven't heard of them plaid. Street art scenester Carles, pickled Banksy Helvetica raw denim Echo Park actually bitters aesthetic. Leggings blog disrupt listicle. Truffaut flannel gastropub readymade, ugh before they sold out sustainable synth Austin Echo Park Vice yr organic. Artisan cronut lumbersexual gluten-free jean shorts. Cold-pressed PBR twee disrupt.
+
+Gentrify Pinterest cray, 90's meh kale chips listicle swag direct trade actually master cleanse. Whatever 3 wolf moon leggings, VHS forage brunch blog ethical hoodie authentic messenger bag farm-to-table kogi DIY mumblecore. Gluten-free PBR&B yr 3 wolf moon, mustache chia XOXO. Pour-over kitsch ugh, normcore gentrify before they sold out Carles chambray locavore stumptown YOLO fixie mumblecore Godard. XOXO sustainable PBR&B seitan. Tote bag shabby chic cardigan skateboard, cold-pressed migas VHS Vice synth four dollar toast Pinterest +1 aesthetic. Slow-carb occupy PBR&B four dollar toast put a bird on it iPhone."
+},
+{
+  title: "Cupcake Ipsum", 
+  body: "Donut ice cream carrot cake powder wafer. Marshmallow tart lemon drops. Lemon drops sugar plum apple pie. Jelly-o cake pudding brownie gummies gummies pudding. Danish candy canes muffin bonbon jelly beans tart cotton candy. Tiramisu dragée soufflé marzipan.
+
+Pie caramels liquorice. Ice cream bonbon pie soufflé icing fruitcake sugar plum cake. Pudding pudding dragée halvah sweet cake brownie. Jelly beans croissant chocolate bar chocolate toffee macaroon liquorice donut jelly. Halvah marzipan soufflé sugar plum biscuit cheesecake. Marshmallow jelly beans gingerbread cheesecake topping bonbon.
+
+Macaroon dragée topping cotton candy. Pastry chocolate cotton candy jelly beans tiramisu cake cake cookie pastry. Chocolate candy gummies. Candy canes tiramisu danish icing cake. Tootsie roll gummi bears pie soufflé cookie muffin carrot cake. Marshmallow sweet roll brownie gingerbread fruitcake cotton candy. Pastry gummi bears ice cream. Sweet roll gummies tiramisu chocolate cake cotton candy. Jelly beans toffee lemon drops soufflé.
+
+Carrot cake cheesecake oat cake chupa chups bonbon. Croissant danish lollipop danish toffee halvah tart fruitcake. Marzipan cheesecake marzipan bear claw candy wafer gingerbread wafer pie. Gingerbread gingerbread liquorice. Marzipan muffin soufflé lollipop jelly beans chupa chups carrot cake. Lemon drops cookie tart dessert bear claw. Cake gummies carrot cake oat cake topping icing cheesecake sweet roll jelly-o. Pie bonbon bonbon. Tootsie roll tootsie roll muffin dragée ice cream sweet.
+
+Cupcake ice cream dessert candy canes apple pie lollipop liquorice. Gummi bears danish macaroon pie pie jujubes macaroon. Lollipop candy canes tiramisu chocolate bar cupcake jujubes. Toffee sweet roll liquorice cupcake gummi bears. Bonbon muffin croissant cotton candy marzipan candy canes. Danish candy canes marzipan cookie lollipop."
+}])
